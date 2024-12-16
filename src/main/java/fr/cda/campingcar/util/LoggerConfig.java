a0a01df9eb54e36c12dd582253cdd7cefca5c36e
major <major.max.englishbull@gmail.com>
2024-12-16 10:41:27 +0100

    Logger File

--- src/main/java/fr/cda/campingcar/util/LoggerConfig.java
+++ src/main/java/fr/cda/campingcar/util/LoggerConfig.java
@@ -15,6 +15,7 @@ import org.apache.logging.log4j.Logger;
 
 public class LoggerConfig {
     private static final Logger LOGGER = LogManager.getLogger(LoggerConfig.class);
+    private static final Logger LOGGER_FILE = LogManager.getLogger("FileLog");
     private static final Logger LOGGER_SCRAPING = LogManager.getLogger("ScrapingLog");
     private static final Logger LOGGER_USER_ACTION = LogManager.getLogger("UserActions");
     private static final Logger LOGGER_DAO_ERROR = LogManager.getLogger("DaoErrors");
@@ -22,6 +23,8 @@ public class LoggerConfig {
     public static Logger getLogger() {
         return LOGGER;
     }
+
+    public static Logger getLoggerFile() {return LOGGER_FILE;}
 
     public static Logger getLoggerScraping() { return LOGGER_SCRAPING; }
 
